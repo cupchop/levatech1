a1f157977d1996437f853d286e47801d3c566741
cupchop <nakahata.shotaro.t1@dc.tohoku.ac.jp>
2022-12-08 20:34:02 +0900

    a

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,9 +9,9 @@ class Post extends Model
 {
     use HasFactory;
     
-    public function getPaginateByLimit(int $limit_count = 5)
+    public function getByLimit(int $limit_count = 10)
 {
     
-    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    return $this->orderby('updated_at', 'DESC')->limit($limit_count)->get();
 }
 }
