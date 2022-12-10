802092185819fd6a38c93143a1c984b65185acee
cupchop <nakahata.shotaro.t1@dc.tohoku.ac.jp>
2022-12-10 15:32:47 +0900

    a

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,17 +5,21 @@
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1>Blog Name</h1>
-        <a href="/posts/create">create</a>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+   
+   
+    </head> 
+    <body class="antialiased">
+        <h1 class='title'>
+            {{ $post->title }}
+        </h1>
+        <div class='content'>
+                <div class='content_post'>
+                    <h3>本文</h3>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
-            @endforeach
+        </div>
+        <div class='footer'>
+             <a href="/">戻る</a>
         </div>
     </body>
 </html>
