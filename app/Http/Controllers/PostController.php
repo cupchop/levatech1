7684df023e1d249ffe0982b99fbcdd3728f3b94b
cupchop <nakahata.shotaro.t1@dc.tohoku.ac.jp>
2022-12-10 15:47:18 +0900

    a

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,11 +9,11 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-      return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);  
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
-    
     public function show(Post $post)
     {
-        return view('posts/show')->with(['post' => $post]);
+        dd($post);
     }
 }
