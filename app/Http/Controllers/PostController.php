ea02442958672c8faf46b6ed941480e05213893a
cupchop <nakahata.shotaro.t1@dc.tohoku.ac.jp>
2022-12-11 16:47:02 +0900

    a

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -37,7 +37,7 @@ class PostController extends Controller
     
     public function update(PostRequest $request, Post $post)
     {
-        $input_post = $request['post'];
+        $input_post=$request['post'];
         $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
     }
